91dc929ef49a914a427b7763fc1db08cca492db4
Joaquin Rosendo <joaquinrosendo@live.com>
2023-10-24 14:40:57 -0500

    create Main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './components/Banner'
 import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   return (
     <div className='app-container'>
       <Banner />
       <Header />
+      <Main />
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Main() {
+    return(
+        <main className="main-container">
+            <h3>About</h3>
+            <article>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</article>
+            <h3>Interests</h3>
+            <article>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</article>
+        </main>
+    )
+}
+
+export default Main;
